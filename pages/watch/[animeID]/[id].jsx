eeabb1a8fdd07727e6fb9d1d1cb3614217c76b51
Dishant Sharma <dishu5570@gmail.com>
2023-04-01 20:00:56 +0530

    ERROR SOLVED

--- pages/watch/[animeID]/[id].jsx
+++ pages/watch/[animeID]/[id].jsx
@@ -11,7 +11,9 @@ function capitalizeAndSplit(inputString) {
   if(inputString){
     let words = inputString.split("-");
     for (let i = 0; i < words.length; i++) {
-      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+      if(words[i]){
+        words[i] = words[i][0]?.toUpperCase() + words[i].slice(1);
+      }
     }
     return words.join(" ");
   }
